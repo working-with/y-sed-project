d02e609f8375d8f7b440b3cfa6235a45ca6daace
joayeon <codinz.n@gmail.com>
2023-12-04 02:20:29 +0900

    design: 끝나는 페이지 디자인

---
+++ src/pages/Finish/index.styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { bodyContainer, flexCenter, height } from '../../style/common';
+
+export const Body = styled.div`
+	${bodyContainer}
+`;
+
+export const Top = styled.div``;
+
+export const Content = styled.div`
+	${height}
+	${flexCenter}
+    flex-direction: column;
+	gap: 140px;
+
+	> img:nth-child(2) {
+		width: 861px;
+		height: 633.361px;
+	}
+`;
