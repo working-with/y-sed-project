69cb59b72943da3c0b4a1a88f273de00f7aa8292
joayeon <codinz.n@gmail.com>
2023-12-04 02:19:34 +0900

    feat: route 추가

--- src/routes/routing.tsx
+++ src/routes/routing.tsx
@@ -7,6 +7,11 @@ import Start from '../pages/Start';
 import Tri from '../pages/Tri';
 import Gui from '../pages/Gui';
 import Exit from '../pages/Exit';
+import Quiz01 from '../pages/Quiz01';
+import Finish from '../pages/Finish';
+import Next from '../pages/Next';
+import So from '../pages/So';
+import Beg from '../pages/Beg';
 
 const router = createBrowserRouter([
 	{
@@ -32,6 +37,27 @@ const router = createBrowserRouter([
 			{
 				path: 'exit',
 				element: <Exit />,
+			},
+			// Quiz list
+			{
+				path: 'quiz01',
+				element: <Quiz01 />,
+			},
+			{
+				path: 'beg',
+				element: <Beg />,
+			},
+			{
+				path: 'so',
+				element: <So />,
+			},
+			{
+				path: 'next',
+				element: <Next />,
+			},
+			{
+				path: 'finish',
+				element: <Finish />,
 			},
 		],
 	},
