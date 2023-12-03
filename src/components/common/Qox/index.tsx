e77f37c4d879de2cc254e7d62ef84aa46ff94330
joayeon <codinz.n@gmail.com>
2023-12-04 02:24:40 +0900

    design: ox 파트 디자인

---
+++ src/components/common/Qox/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './index.styled';
+
+function Qox() {
+	return (
+		<S.Body>
+			<div>
+				<img src="/assets/img/icon/blueO.svg" />
+				<img src="/assets/img/icon/redX.svg" />
+			</div>
+		</S.Body>
+	);
+}
+
+export default Qox;
