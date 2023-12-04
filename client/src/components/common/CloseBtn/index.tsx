6430c42fcd18a338587b6015d0c3db7a495dedcf
joayeon <codinz.n@gmail.com>
2023-12-05 07:39:01 +0900

    feat: closeBtn 클릭 시 추가

--- client/src/components/common/CloseBtn/index.tsx
+++ client/src/components/common/CloseBtn/index.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import * as S from './index.styled';
 
 function CloseBtn() {
-	return <S.Image src="/assets/img/icon/grayX.svg" />;
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		if (window.confirm('설문 조사를 그만하시겠습니까?')) navigate('/');
+		else return;
+	};
+
+	return <S.Image src="/assets/img/icon/grayX.svg" onClick={handleClick} />;
 }
 
 export default CloseBtn;
