a3a236695ddaaa0c848d78a1794a8db6d57f33c1
minenam <nhm6947@gmail.com>
2023-12-07 05:45:20 +0900

    feat: swagger 초기 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5000;
 
-  
+  const config = new DocumentBuilder()
+    .setTitle('y-sed project API Document')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/document', app, document);
+
   await app.listen(port);
 }
 bootstrap();
