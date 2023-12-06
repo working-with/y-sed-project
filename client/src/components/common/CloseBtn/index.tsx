704b105e485094cfa8a44e65486100023fef3ea9
joayeon <codinz.n@gmail.com>
2023-12-06 23:46:04 +0900

    feat: closeBtn 버튼 로직 추가

--- client/src/components/common/CloseBtn/index.tsx
+++ client/src/components/common/CloseBtn/index.tsx
@@ -5,8 +5,7 @@ function CloseBtn() {
 	const navigate = useNavigate();
 
 	const handleClick = () => {
-		if (window.confirm('설문 조사를 그만하시겠습니까?')) navigate('/');
-		else return;
+		navigate('/exit');
 	};
 
 	return <S.Image src="/assets/img/icon/grayX.svg" onClick={handleClick} />;
