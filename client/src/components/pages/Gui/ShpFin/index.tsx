1000988738dc389bf68c2510ef401b6902fd7268
joayeon <codinz.n@gmail.com>
2023-12-07 22:03:55 +0900

    feat: 시작하기 화면에서 페이지 이동 설정

--- client/src/components/pages/Gui/ShpFin/index.tsx
+++ client/src/components/pages/Gui/ShpFin/index.tsx
@@ -1,27 +1,31 @@
-import * as S from './index.styled';
+import * as S from "./index.styled";
 
-import Bottom from '../../../common/Bottom';
-import Button from '../../../common/Button';
+import Bottom from "../../../common/Bottom";
+import Button from "../../../common/Button";
+import { useNavigate } from "react-router-dom";
 
 function ShpFin() {
-	return (
-		<>
-			<S.Content>
-				<Button variant="green">시작하기</Button>
-			</S.Content>
+  const navigate = useNavigate();
 
-			<Bottom button={true}>
-				대단한데?
-				<br />
-				우리 이제 시작해볼까?
-			</Bottom>
-		</>
-	);
+  const handleStartClick = () => {
+    navigate("/beg");
+  };
+
+  return (
+    <>
+      <S.Content>
+        <Button variant="green" onClick={handleStartClick}>
+          시작하기
+        </Button>
+      </S.Content>
+
+      <Bottom>
+        대단한데?
+        <br />
+        우리 이제 시작해볼까?
+      </Bottom>
+    </>
+  );
 }
 
 export default ShpFin;
-
-// Shp11
-// 와! 좋았어.
-// 다음으로 가볼까?
-// color='bluePlay'
