34dd9936dbd313ef0480c2f97fe568cce4b6e3a4
joayeon <codinz.n@gmail.com>
2023-12-07 22:01:50 +0900

    feat: message 상수 추가

---
+++ client/src/constants/message.ts
@@ -0,0 +1,10 @@
+const MESSAGE = {
+	CHECK: {
+		NAME: '등록된 이름이 없습니다. 이름을 입력해 주세요.',
+		CODE: '등록된 코드 번호가 없습니다. 코드 번호를 입력해 주세요.',
+		GENDER: '등록된 성별이 없습니다. 성별을 입력해 주세요.',
+	},
+	EXCEL_DOWN: '아동 전체 실험 정보를 엑셀로 다운하시겠습니까?',
+} as const;
+
+export default MESSAGE;
