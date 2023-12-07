0271c26fcd969788535771ff3f9910b716a05187
joayeon <codinz.n@gmail.com>
2023-12-07 22:05:13 +0900

    feat: graph 클릭 시 색상 변경 가능하게 설정

--- client/src/components/common/Graph/index.tsx
+++ client/src/components/common/Graph/index.tsx
@@ -1,14 +1,41 @@
-import * as S from './index.styled';
+// Graph 컴포넌트
+import * as S from "./index.styled";
+import { useState } from "react";
 
-function Graph() {
-	return (
-		<S.Body>
-			<S.First />
-			<S.Second />
-			<S.Third />
-			<S.Fourth />
-		</S.Body>
-	);
+export interface GraphProps {
+  onGraphItemClick?: (index: number) => void;
+  clicked?: boolean;
+}
+
+function Graph({ onGraphItemClick }: GraphProps) {
+  const [clickedIndices, setClickedIndices] = useState<number[]>([]);
+
+  const handleGraphItemClickInternal = (index: number) => {
+    // 클릭된 상태를 토글
+    const updatedIndices = [...clickedIndices];
+    const indexInArray = updatedIndices.indexOf(index);
+
+    if (indexInArray === -1) {
+      updatedIndices.push(index);
+    } else {
+      updatedIndices.splice(indexInArray, 1);
+    }
+
+    setClickedIndices(updatedIndices);
+
+    if (onGraphItemClick) {
+      onGraphItemClick(index);
+    }
+  };
+
+  return (
+    <S.Body>
+      <S.First clicked={clickedIndices.includes(0)} onClick={() => handleGraphItemClickInternal(0)} />
+      <S.Second clicked={clickedIndices.includes(1)} onClick={() => handleGraphItemClickInternal(1)} />
+      <S.Third clicked={clickedIndices.includes(2)} onClick={() => handleGraphItemClickInternal(2)} />
+      <S.Fourth clicked={clickedIndices.includes(3)} onClick={() => handleGraphItemClickInternal(3)} />
+    </S.Body>
+  );
 }
 
 export default Graph;
