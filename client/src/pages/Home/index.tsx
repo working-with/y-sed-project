32e9d96af0e6ca947c170a032c94b61015d5cd6a
joayeon <codinz.n@gmail.com>
2023-12-07 22:01:16 +0900

    fix: 시작 화면 api 연결 로직 추가

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 
 import { userInfoAtom } from '../../recoil/atoms/user.atom';
+import useCloseBtn from '../../hooks/useCloseBtn';
+import validateInput from '../../utils/validateInput';
+import axiosRequest from '../../api';
+import { KidInformation, ResData } from '../../@types';
 
 import * as S from './index.styled';
-
 import Button from '../../components/common/Button';
-import useCloseBtn from '../../hooks/useCloseBtn';
+import MESSAGE from '../../constants/message';
 
 function Home() {
 	useCloseBtn();
+
+	const navigate = useNavigate();
 
 	const [userInfo, setUserInfo] = useRecoilState(userInfoAtom);
 
@@ -22,7 +27,32 @@ function Home() {
 		}));
 	};
 
-	const handleStartClick = () => {};
+	const handleStartClick = async () => {
+		const fail = validateInput(userInfo).filter(info => info.checked);
+
+		if (fail.length > 0) {
+			const errorMessage = fail[0].message;
+			alert(errorMessage);
+		} else {
+			// const response = await axiosRequest.requestAxios<ResData<KidInformation>>(
+			// 	'post',
+			// 	'/v1/kid',
+			// );
+			// const data = response.data;
+			// console.log(data);
+			navigate('/start');
+		}
+	};
+
+	const handleExcelDownClick = async () => {
+		if (window.confirm(MESSAGE.EXCEL_DOWN)) {
+			const response = await axiosRequest.requestAxios<
+				ResData<KidInformation[]>
+			>('get', '/v1/kid');
+
+			const data = response.data;
+		}
+	};
 
 	return (
 		<S.Body>
@@ -49,11 +79,13 @@ function Home() {
 				</S.Wrapper>
 
 				<S.ButtonBox>
-					<Button variant="green">엑셀 다운</Button>
+					<Button variant="green" onClick={handleExcelDownClick}>
+						엑셀 다운
+					</Button>
 
-					<Link to="/start">
-						<Button variant="blue">시작하기</Button>
-					</Link>
+					<Button variant="blue" onClick={handleStartClick}>
+						시작하기
+					</Button>
 				</S.ButtonBox>
 			</S.Content>
 		</S.Body>
