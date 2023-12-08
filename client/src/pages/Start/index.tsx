b5b52a64c509ac1199bb75e37fb44726d9604aa6
joayeon <codinz.n@gmail.com>
2023-12-08 23:52:19 +0900

    fix: 이름 나오는 부분 변경

--- client/src/pages/Start/index.tsx
+++ client/src/pages/Start/index.tsx
@@ -1,41 +1,44 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
-import { userInfoAtom } from '../../recoil/atoms/user.atom';
-import getName from '../../utils/getName';
+import { userInfoAtom } from "../../recoil/atoms/user.atom";
+import getName from "../../utils/getName";
 
-import * as S from './index.styled';
+import * as S from "./index.styled";
 
-import Button from '../../components/common/Button';
-import useCloseBtn from '../../hooks/useCloseBtn';
+import Button from "../../components/common/Button";
+import useCloseBtn from "../../hooks/useCloseBtn";
 
 function Start() {
-	useCloseBtn();
+  useCloseBtn();
 
-	const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-	const onClick = () => {
-		setClick(true);
-	};
+  const onClick = () => {
+    setClick(true);
+  };
 
-	const userInfo = useRecoilValue(userInfoAtom);
-	const { name } = getName(userInfo.name);
+  const userInfo = useRecoilValue(userInfoAtom);
+  const name = getName(userInfo.name);
 
-	return (
-		<S.Body>
-			<S.TitleBox>
-				{!click && <img src="/assets/img/icon/smileIcon.svg" />}
-				{click && <span>안녕 {name}~!</span>}
-			</S.TitleBox>
+  const userInitials = name.slice(0, 2);
+  const kidName = name.includes("이") ? `${userInitials}아` : `${userInitials}야`;
 
-			<S.ButtonBox>
-				<Button variant="blue" onClick={onClick}>
-					{click ? <Link to="/tri">안녕?</Link> : '시작하기'}
-				</Button>
-			</S.ButtonBox>
-		</S.Body>
-	);
+  return (
+    <S.Body>
+      <S.TitleBox>
+        {!click && <img src="/assets/img/icon/smileIcon.svg" />}
+        {click && <span>안녕 {kidName}~!</span>}
+      </S.TitleBox>
+
+      <S.ButtonBox>
+        <Button variant="blue" onClick={onClick}>
+          {click ? <Link to="/tri">안녕?</Link> : "시작하기"}
+        </Button>
+      </S.ButtonBox>
+    </S.Body>
+  );
 }
 
 export default Start;
