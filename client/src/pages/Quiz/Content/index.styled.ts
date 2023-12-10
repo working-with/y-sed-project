3c71159a3cc1841c51c7187a2cfb5c94359a3f42
joayeon <codinz.n@gmail.com>
2023-12-10 17:00:41 +0900

    design: quiz content 디자인 수정

--- client/src/pages/Quiz/Content/index.styled.ts
+++ client/src/pages/Quiz/Content/index.styled.ts
@@ -16,3 +16,24 @@ export const Content = styled.div<{ quiz?: boolean }>`
     height: 862px;
   }
 `;
+
+export const Qox = styled.div`
+  position: absolute;
+  z-index: 1;
+  background: rgba(0, 0, 0, 0.75);
+  width: 1401px;
+  height: 870px;
+  margin: 0 auto;
+  transform: translate(-0%, 5.9%);
+
+  > div {
+    position: fixed;
+    ${flexCenter}
+    gap: 137px;
+    transform: translate(25%, 50%);
+  }
+
+  & img:hover {
+    transform: scale(1.1);
+  }
+`;
