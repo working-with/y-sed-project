ce0cab13f015211787bb32431643852aab77e897
minenam <nhm6947@gmail.com>
2023-12-10 12:32:56 +0900

    doc: fix typo in swagger url

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/document', app, document);
+  SwaggerModule.setup('api/v1/document', app, document);
 
   await app.listen(port);
 }
