219e2d772b4ba4731ba022a1062e7c139a784a91
joayeon <codinz.n@gmail.com>
2023-12-11 06:31:14 +0900

    fix: statusbar 상태 변경 로직 수정

--- client/src/pages/Quiz/Next/index.tsx
+++ client/src/pages/Quiz/Next/index.tsx
@@ -1,15 +1,11 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import * as S from "./index.styled";
 
 import Bottom from "../../../components/common/Bottom";
 import Button from "../../../components/common/Button";
-
-const imgSrc = {
-  q1: "/assets/img/icon/Q2.svg",
-  q2: "/assets/img/icon/Q3.svg",
-  q3: "/assets/img/icon/100.svg",
-};
+import StatusBar from "../../../components/common/StatusBar";
 
 function Next() {
   const navigate = useNavigate();
@@ -25,11 +21,17 @@ function Next() {
     }
   };
 
+  const [status, setStatus] = useState<number>(0);
+
+  useEffect(() => {
+    setStatus(Number(params.oxId));
+  }, []);
+
   return (
     <S.Body>
       <S.Content>
         <S.Top>
-          <img src={imgSrc.q1} />
+          <StatusBar status={status} />
         </S.Top>
 
         <S.Middle>
