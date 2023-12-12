0ca49062f9dde18ebcd4e4fa3a8ceb8b3800d81e
joayeon <codinz.n@gmail.com>
2023-12-13 07:57:36 +0900

    fix: edit message

--- client/src/constants/message.ts
+++ client/src/constants/message.ts
@@ -4,7 +4,7 @@ const MESSAGE = {
     CODE: "등록된 코드 번호가 없습니다. 코드 번호를 입력해 주세요.",
     GENDER: "등록된 성별이 없습니다. 성별을 입력해 주세요.",
   },
-  EXCEL_DOWN: "아동 전체 실험 정보를 엑셀로 다운하시겠습니까?",
+  EXCEL_DOWN: "아동 전체 실험 정보를 엑셀로 내려받으시겠습니까?",
 } as const;
 
 export default MESSAGE;
