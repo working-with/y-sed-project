87bfc06aa27e6cd5bced758df8783215c9f36093
joayeon <codinz.n@gmail.com>
2023-12-12 22:30:08 +0900

    fix: experiment type 수정

--- client/src/@types/Experiment.ts
+++ client/src/@types/Experiment.ts
@@ -1,22 +1,20 @@
+import { Image } from "./Image";
+
 export interface Experiment {
-  id: number;
+  data: data[];
+}
+
+interface data {
+  id: string;
   code: string;
-  image: string[];
-  answerType: string;
-  booleanAnswer?: BooleanAnswer;
-  scaleAnswer?: number;
+  gender: number;
+  image: Image[];
+  // answerType: string;
+  // booleanAnswer?: BooleanAnswer;
+  // scaleAnswer?: number;
 }
 
 enum BooleanAnswer {
   "O" = 0,
   "X" = 1,
 }
-
-// interface Experiment {
-// 	id: number (auto)
-// 	code: string (ex. “G_0123”)
-// 	image: Array<Image>
-// 	answerType: string ( “boolean”, “scale” )
-// 	booleanAnswer?: enum (O:0, X:1)
-// 	scaleAnswer?: number (1~4)
-// }
