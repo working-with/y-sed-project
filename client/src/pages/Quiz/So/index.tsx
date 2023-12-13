0d24515279640788a141fd47f1c535f1f63c6d4d
joayeon <codinz.n@gmail.com>
2023-12-13 22:54:07 +0900

    fix: scale 입력 작동되는지 로직 확인

--- client/src/pages/Quiz/So/index.tsx
+++ client/src/pages/Quiz/So/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { userInfoAtom } from "../../../recoil/atoms/user.atom";
 import common from "../../../utils/common";
 import getName from "../../../utils/getName";
+import { quizAnswerAtom } from "../../../recoil/atoms/quiz.atom";
 
 import * as S from "./index.styled";
 
@@ -16,21 +17,38 @@ function So() {
   const navigate = useNavigate();
   const params = useParams();
 
+  const { experimentId, oxId } = params;
+
   const userInfo = useRecoilValue(userInfoAtom);
   const name = getName(userInfo.name);
 
   const { SO } = common(name);
 
+  const [quizAnswer, setQuizAnswer] = useRecoilState(quizAnswerAtom);
+
   const handleBtnClick = () => {
-    if (params.experimentId === "9" && params.oxId === "2") {
-      navigate("/finish");
-    } else navigate(`/quiz/${params.experimentId}/next/${params.oxId}`);
+    if (selectedIndex !== null) {
+      const newQuiz = {
+        [`${experimentId}-${Number(oxId) + 1}`]: {
+          booleanAnswer: 1,
+          scaleAnswer: (selectedIndex && selectedIndex + 1) || (selectedIndex === 0 && selectedIndex + 1),
+        },
+      };
+
+      setQuizAnswer((prev: any) => [...prev, newQuiz]);
+
+      if (experimentId === "9" && oxId === "2") {
+        navigate("/finish");
+      } else navigate(`/quiz/${experimentId}/next/${oxId}`);
+    } else {
+      alert("그래프를 클릭해 주세요!");
+    }
   };
 
   const [status, setStatus] = useState<number>(0);
 
   useEffect(() => {
-    setStatus(Number(params.oxId));
+    setStatus(Number(oxId));
   }, []);
 
   // graph click
@@ -39,10 +57,6 @@ function So() {
   const handleGraphClick = (index: number) => {
     setSelectedIndex(prevIndex => (prevIndex === index ? null : index));
   };
-
-  useEffect(() => {
-    console.log(selectedIndex);
-  }, [selectedIndex]);
 
   return (
     <S.Body>
@@ -54,7 +68,7 @@ function So() {
         <Graph onClick={handleGraphClick} index={selectedIndex} />
       </S.Content>
 
-      <Bottom button={true} color="bluePlay" onClick={handleBtnClick}>
+      <Bottom button={true} color={selectedIndex === null ? "" : "bluePlay"} onClick={handleBtnClick}>
         {SO}
       </Bottom>
     </S.Body>
