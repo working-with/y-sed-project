c9037a19dcddf10944685e1d23b2e5603d279d9e
minenam <nhm6947@gmail.com>
2023-12-13 17:39:22 +0900

    feat: set clova service

---
+++ server/src/clova/clova.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import { VoiceType } from 'src/voice/dto/voice.dto';
+
+@Injectable()
+export class ClovaService {
+  private readonly clovaConfig;
+
+  constructor(private readonly configService: ConfigService) {
+    this.clovaConfig = {
+      clovaUrl: this.configService.get<string>('CLOVA_REQUEST_URL'),
+      clientId: this.configService.get<string>('CLOVA_CLIENT_ID'),
+      cliendtSecret: this.configService.get<string>('CLOVA_CLIENT_SECRET'),
+    };
+  }
+
+  async getClovaTTS(voiceType: VoiceType, text: string) {
+    const clovaUrl = `${this.clovaConfig.clovaUrl}`;
+    const data = {
+      speaker: voiceType,
+      text: text,
+      format: 'mp3',
+    };
+    const response = await axios.post(clovaUrl, data, {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-NCP-APIGW-API-KEY-ID': this.clovaConfig.clientId,
+        'X-NCP-APIGW-API-KEY': this.clovaConfig.cliendtSecret,
+      },
+    });
+    console.log(response.headers);
+    return response;
+  }
+}
