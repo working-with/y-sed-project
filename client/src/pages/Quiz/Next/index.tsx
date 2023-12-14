061b7939519d9a870dea0780f47f8b71afcb1ce5
joayeon <codinz.n@gmail.com>
2023-12-14 19:44:43 +0900

    fix: next 페이지 tts 로직 수정

--- client/src/pages/Quiz/Next/index.tsx
+++ client/src/pages/Quiz/Next/index.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
-import getRandomNext from "../../../utils/getNextMessage";
+import getNextMessage from "../../../utils/getNextMessage";
 import NEXT_MESSAGE from "../../../constants/nextMessage";
+import axiosRequest from "../../../api";
+import { ResData } from "../../../@types";
+import { userInfoAtom } from "../../../recoil/atoms/user.atom";
 
 import * as S from "./index.styled";
 
@@ -26,8 +30,51 @@ function Next() {
     }
   };
 
+  const userInfo = useRecoilValue(userInfoAtom);
+
+  const [currentTTS, setCurrentTTS] = useState(0);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const text = [numberOxId === 2 ? NEXT_MESSAGE[experiment] : getNextMessage(experiment, numberOxId)];
+
+  useEffect(() => {
+    const currentAudio = audioRef.current;
+
+    const plusCurrentTTS = () => {
+      setCurrentTTS((prev: number) => prev + 1);
+    };
+
+    if (currentAudio && text[currentTTS]) {
+      currentAudio.addEventListener("ended", plusCurrentTTS);
+
+      const getVoice = async () => {
+        const postData = {
+          name: userInfo.name,
+          voiceType: userInfo.gender ? "nhajun" : "vdain",
+          script: text[currentTTS],
+        };
+
+        const response = await axiosRequest.requestAxios<ResData<Blob>>("post", "/v1/voice", postData);
+        const data = response.data;
+
+        const url = URL.createObjectURL(new Blob([data]));
+
+        currentAudio.src = url;
+        currentAudio.play().catch(e => console.log(e));
+      };
+
+      getVoice();
+
+      return () => {
+        currentAudio.removeEventListener("ended", plusCurrentTTS);
+      };
+    }
+  }, [audioRef.current, currentTTS]);
+
   return (
     <S.Body>
+      <audio ref={audioRef} />
+
       <S.Content>
         <S.Top>
           <StatusBar status={numberOxId} />
@@ -40,7 +87,7 @@ function Next() {
         </S.Middle>
       </S.Content>
 
-      <Bottom>{numberOxId === 2 ? NEXT_MESSAGE[experiment] : getRandomNext()}</Bottom>
+      <Bottom>{text[0]}</Bottom>
     </S.Body>
   );
 }
