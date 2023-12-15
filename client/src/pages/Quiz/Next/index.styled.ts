fde634ab398a481d45264b77babb73cabc6d8d9c
joayeon <codinz.n@gmail.com>
2023-12-15 22:33:28 +0900

    design: 이모티콘에 페이드 아웃 효과 추가

--- client/src/pages/Quiz/Next/index.styled.ts
+++ client/src/pages/Quiz/Next/index.styled.ts
@@ -1,5 +1,16 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { bodyContainer, flexCenter, height } from "../../../style/common";
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+    transform:translate(50%, -40%) scale(1); /* 초기 크기 */
+  }
+  100% {
+    opacity: 0;
+    transform:translate(50%, -40%) scale(1.4); /* 크기를 커지게 설정 (원하는 크기로 수정) */
+  }
+`;
 
 export const Body = styled.div`
   ${bodyContainer}
@@ -26,5 +37,9 @@ export const ImageBox = styled.div`
     height: 500px;
     position: fixed;
     transform: translate(50%, -40%);
+    animation: ${fadeOut} 2s 1 ease alternate;
+
+    opacity: 0; /* 초기에 투명하게 설정 */
+    animation-fill-mode: forwards; /* 애니메이션 종료 후에 상태 유지 */
   }
 `;
