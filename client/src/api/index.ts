95f21e5fb1ea0fad482c10ed6820e87d9630e78a
minenam <nhm6947@gmail.com>
2023-12-16 08:07:27 +0900

    build: change production server url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -5,7 +5,7 @@ const allowMethod: string[] = ["get", "post", "put", "patch", "delete"];
 
 // Axios 인스턴스 생성
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://y-sed-web.azurewebsites.net/api",
   timeout: 5000,
   // responseType: "blob",
 });
