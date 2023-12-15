93a45d0481b2665ead2ecf7c8f6ab70e56dff5a4
joayeon <codinz.n@gmail.com>
2023-12-15 22:33:07 +0900

    fix: user atom 수정

--- client/src/recoil/atoms/user.atom.tsx
+++ client/src/recoil/atoms/user.atom.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
@@ -7,9 +7,20 @@ export const userInfoAtom = atom({
   key: "userInfo",
   default: {
     kidId: "",
-    name: "",
     code: "",
     gender: "",
+    firstName: "",
+    lastName: "",
   },
   effects_UNSTABLE: [persistAtom],
+});
+
+export const userSelector = selector({
+  key: "userInfoEdit",
+  get: ({ get }) => {
+    const userInfo = get(userInfoAtom);
+    const { firstName, lastName, code, gender } = userInfo;
+
+    return { name: `${firstName}${lastName}`, code, gender };
+  },
 });
